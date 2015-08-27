5758786b75d5d5231059f672206e0f5e188e05b4
johnwquarles <john.w.quarles@gmail.com>
2015-08-27 14:34:10 -0500

    websockets

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 'use strict';
 
 var express = require('express');
+var socketio = require('socket.io');
 
 var app = express();
 
 app.use(express.static('client'));
 
-app.listen(3000, function () {
+// need to take listening server & pass it into socket.io
+var server = app.listen(3000, function () {
   console.log('Server listening on port 3000');
 });
+
+socketio(server);
