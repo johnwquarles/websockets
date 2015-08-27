a7f7219ae388aa9cc09aebe9d817b9ca5bf041a4
johnwquarles <john.w.quarles@gmail.com>
2015-08-27 15:57:34 -0500

    simple chat app works

--- server/app.js
+++ server/app.js
@@ -12,4 +12,26 @@ var server = app.listen(3000, function () {
   console.log('Server listening on port 3000');
 });
 
-socketio(server);
+var io = socketio(server);
+
+io.on('connection', function (socket) {
+  console.log('Client connected:', socket.id);
+
+  socket.on('disconnect', function () {
+    console.log('Client disconnected:', socket.id);
+  })
+
+  socket.on('chatMessage', function (msg) {
+    console.log('Chat Message received:', msg);
+
+    // below: toOne, toOthers & toEveryone are arbitrary.
+
+    // // to the socket we got it from
+    // socket.emit('chatMessage', {toOne: msg});
+    // // to everyone except that socket (all other connected sockets)
+    // socket.broadcast.emit('chatMessage', {toOthers: msg});
+    // to all connected sockets (everyone)
+    io.emit('chatMessage', msg);
+  })
+
+});
